eb1c453c7ab99c9e3363a8d39b342a75df02b5ef
Nitesh Rai <nitesh.rai@hotmail.com>
2020-02-02 14:29:54 +0000

    added atp tournament winners table

---
+++ RandomLearning/dbo/Tables/ATPWinners.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ATPWinners]
+(
+	[Id] INT NOT NULL PRIMARY KEY
+	,[Tournament_Name] Varchar(100)
+	,[Year] Int
+	,[Winner Last Name] NVarchar(100)
+	,[Champion] NVarchar(200)
+)
